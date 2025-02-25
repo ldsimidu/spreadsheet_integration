49aacbf5b460ad052c60ebc18ef0f1eac83f63bc
Lucas Simidu <lucas.simidu@outlook.com>
2025-02-25 09:18:02 -0300

    FEAT: Adding table to a web page w/Flask

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import pandas as pd
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 caminho_excel = "spreadsheets/tests-ss.xlsx"
 
 pf = pd.read_excel(caminho_excel)
-print(pf)
+#print(pf)
+
+@app.route("/")
+def show_table():
+    pf = pd.read_excel(caminho_excel)
+    tabela_html = pf.to_html(classes="table table-striped")
+    
+    return render_template("index.html", tabela=tabela_html)
+
+if __name__ == "__main__":
+    app.run(debug=True)
